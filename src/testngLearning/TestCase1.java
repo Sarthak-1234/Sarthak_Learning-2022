14b014521f4aece79a2c441edc1546c19a7c19c8
Sarthak <sarthak.mzn@gmail.com>
2022-05-16 12:38:13 +0530

    adding testng first testfile

---
+++ src/testngLearning/TestCase1.java
@@ -0,0 +1,27 @@
+package testngLearning;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Test3");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("Test4");
+	}
+
+}
