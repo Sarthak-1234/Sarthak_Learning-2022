b5afa1827635eacb51817429d0986fa471ea8100
Sarthak <sarthak.mzn@gmail.com>
2022-05-19 22:54:09 +0530

    added test2

---
+++ src/testngLearning/TestCase2.java
@@ -0,0 +1,31 @@
+package testngLearning;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+	
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+		
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test2");
+		
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Test3");
+		
+	}
+	
+	@Test
+	public void test4() {
+		
+		
+	}
+
+}
