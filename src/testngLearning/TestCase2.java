40397e1a6e9f5c316df90c3968b71059f93b99e2
Sarthak <sarthak.mzn@gmail.com>
2022-05-19 23:05:47 +0530

    added 3 and removed 1 and 2

--- src/testngLearning/TestCase2.java
+++ src/testngLearning/TestCase2.java
@@ -7,20 +7,21 @@ public class TestCase2 {
 	@Test
 	public void test1() {
 		System.out.println("Test1");
-		//Adding through github
+		
 		
 	}
 	
 	@Test
 	public void test2() {
 		System.out.println("Test2");
-		//Added through git bash
+		
 		
 	}
 	
 	@Test
 	public void test3() {
 		System.out.println("Test3");
+		//Added test 3 and removing test1 and test 2
 		
 	}
 	
